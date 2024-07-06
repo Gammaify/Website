b464bad67a3c06d7629645e90be645b5ff262f13
Blugamma <blugammadesign@gmail.com>
2024-07-06 02:32:42 +0100

    readded database code

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -15,50 +15,50 @@ const access: ConnectionOptions = {
 
 
 export default defineEventHandler(async (event) => {
-    // const data = await readBody(event);
+    const data = await readBody(event);
 
-    // console.log(data)
-    // const connection = await mysql.createConnection(access);
-    // const query = getQuery(event);
-    // try{
-    //     const [results] = await connection.query(
-    //         'INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)',
-    //         [data.name, data.email, data.message]
-    //     )  
-    //     var resultJson = JSON.parse(JSON.stringify(results));
-    //     if (resultJson.affectedRows > 0) {
-    //         return 'success'
-    //     }
-    //     else{
-    //         return 'error'
-    //     }
+    console.log(data)
+    const connection = await mysql.createConnection(access);
+    const query = getQuery(event);
+    try{
+        const [results] = await connection.query(
+            'INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)',
+            [data.name, data.email, data.message]
+        )  
+        var resultJson = JSON.parse(JSON.stringify(results));
+        if (resultJson.affectedRows > 0) {
+            return 'success'
+        }
+        else{
+            return 'error'
+        }
         
         
-    // } catch(err) {
-    //    return 'error'
-    // }
+    } catch(err) {
+       return 'error'
+    }
 
 
-    var transport = nodemailer.createTransport({
-        host: "localhost",
-        port: 587,
-        auth: {
-        user: "smtp@gammaify.com",
-        pass: "Megaquest1"
-        }
-    });
+    // var transport = nodemailer.createTransport({
+    //     host: "localhost",
+    //     port: 587,
+    //     auth: {
+    //     user: "smtp@gammaify.com",
+    //     pass: "Megaquest1"
+    //     }
+    // });
 
-    var mailOptions = {
-        from: '"Support Team" <noreply@gammaify.com>',
-        to: 'oliver@gammaify.com',
-        subject: 'Test Email',
-        html: 'Test email sent successfully.',
-    };
+    // var mailOptions = {
+    //     from: '"Support Team" <noreply@gammaify.com>',
+    //     to: 'oliver@gammaify.com',
+    //     subject: 'Test Email',
+    //     html: 'Test email sent successfully.',
+    // };
 
-    transport.sendMail(mailOptions, (error: Error|null, info: SentMessageInfo) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Successfully sent');
-    });
+    // transport.sendMail(mailOptions, (error: Error|null, info: SentMessageInfo) => {
+    //     if (error) {
+    //         return console.log(error);
+    //     }
+    //     console.log('Successfully sent');
+    // });
 })
